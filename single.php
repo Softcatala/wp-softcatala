1d6b5a613b31598b53852aaa031e012c2d518973
Pau Iranzo <paugnu@gmail.com>
2015-09-24 18:32:53 +0200

    #4 Add widgets to noticias sidebar + new links system

--- single.php
+++ single.php
@@ -13,6 +13,17 @@ $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
 $context['comment_form'] = TimberHelper::get_comment_form();
+$post_links = types_child_posts('link', $post->ID);
+$links = array();
+foreach ($post_links as $k => $post_link) {
+	$links[]['link_title'] = $post_link->fields['link_title'];
+	$links[]['link_url'] = $post_link->fields['link_url'];
+	$links[]['link_description'] = $post_link->fields['link_description'];  
+}
+
+$context['links'] = $links;
+$context['sidebar_top'] = Timber::get_widgets('sidebar_top');
+$context['sidebar_bottom'] = Timber::get_widgets('sidebar_bottom');
 
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
