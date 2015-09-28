8b2a66e13a62d15755f3daa37eb76327623360d5
Pau Iranzo <paugnu@gmail.com>
2015-09-28 17:04:24 +0200

    #7 Corrector templates, forms, languagetool libs

---
+++ corrector.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Corrector Softcatala
+ *
+ * @package wp-softcatala
+ */
+
+/* JS scripts */
+wp_enqueue_script( 'sc-js-corrector-1', get_template_directory_uri() . '/inc/languagetool/online-check/tiny_mce/tiny_mce.js', array(), '1.0.0', true );
+wp_enqueue_script( 'sc-js-corrector-2', get_template_directory_uri() . '/inc/languagetool/online-check/tiny_mce/plugins/atd-tinymce/editor_plugin.js', array(), '1.0.0', true );
+wp_enqueue_script( 'sc-js-corrector-3', get_template_directory_uri() . '/inc/languagetool/js/ZeroClipboard.js', array(), '1.0.0', true );
+wp_enqueue_script( 'sc-js-corrector-4', get_template_directory_uri() . '/inc/languagetool/js/jquery.metacookie.js', array(), '1.0.0', true );
+wp_enqueue_script( 'sc-js-corrector-5', get_template_directory_uri() . '/static/js/languagetool.js', array(), '1.0.0', true );
+wp_enqueue_script( 'sc-js-corrector-6', get_template_directory_uri() . '/static/js/jquery.fancybox-2.1.5.pack.js', array(), '1.0.0', true );
+wp_enqueue_style( 'sc-css-corrector', get_template_directory_uri() . '/static/css/languagetool.css', array(), '1.0' );
+
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$post_links = types_child_posts('link');
+$context['links'] = $post->get_field( 'link' );
+$context['sidebar_top'] = Timber::get_widgets('sidebar_top');
+$context['sidebar_bottom'] = Timber::get_widgets('sidebar_bottom');
+Timber::render( array( 'corrector.twig' ), $context );
---
+++ static/js/languagetool.js
@@ -0,0 +1,212 @@
+		var SC_COOKIE = 'sc-languagetool';
+        
+        (function($){
+        $(document).ready(function() {
+			$("a.fancyboxImage").fancybox({
+				'hideOnContentClick': true,
+				'titlePosition': 'inside'
+			});
+
+			$('#submit').click(function() {
+				doit();
+				return false;
+			});
+			
+			$('#check_formes_generals').click(function() {
+				$('#opcions_valencia').hide();
+			});
+			
+			$('#check_formes_valencianes').click(function() {
+				$('#opcions_valencia').show();
+			});
+			
+			$('#check_formes_balears').click(function() {
+				$('#opcions_valencia').hide();
+			});
+			
+			$('#text_prova').click(function() {
+				insertDemoText();
+			});
+			
+			var clip = new ZeroClipboard( $("#copyclip"), {
+				  moviePath: "/themes/wp-softcatala/inc/languagetool/js/ZeroClipboard.swf"
+			});
+			
+			clip.on( 'mouseover', function ( client, args ) {
+				clip.setText($.trim(tinymce.editors[0].core.getPlainText()));
+			});
+            
+            read_cookie_status();
+		});
+		
+		tinyMCE.init({
+			mode : "specific_textareas",
+			editor_selector : "lt",
+			plugins : "AtD,paste",
+
+			//Keeps Paste Text feature active until user deselects the Paste as Text button
+			paste_text_sticky : true,
+			//select pasteAsPlainText on startup
+			setup : function(ed) {
+				ed.onInit.add(function(ed) {
+					ed.pasteAsPlainText = true;
+				});
+			},
+
+			/* translations: */
+			languagetool_i18n_no_errors :
+			{
+				'ca': 'No s\'ha trobat cap error'
+			},
+			languagetool_i18n_explain :
+			{
+				// "Explain..." - shown if there's an URL with a more detailed description:
+				'ca': 'Més informació…'
+			},
+			languagetool_i18n_ignore_once :
+			{
+				'ca': 'Ignora el suggeriment'
+			},
+			languagetool_i18n_ignore_all :
+			{
+				'ca': 'Ignora aquesta classe d\'errors'
+			},
+
+			languagetool_i18n_current_lang :    function() { return document.checkform.lang.value; },
+			/* the URL of your proxy file: */
+			//languagetool_rpc_url                 : "/languagetool/online-check/tiny_mce/plugins/atd-tinymce/server/proxy.php?url=",
+			languagetool_rpc_url                 : "http://www.softcatala.org/languagetool/api/checkDocument",
+			/* edit this file to customize how LanguageTool shows errors: */
+			languagetool_css_url                 : "/themes/wp-softcatala/inc/languagetool/online-check/tiny_mce/plugins/atd-tinymce/css/content.css",
+			/* this stuff is a matter of preference: */
+			height                             : 300,
+			theme                              : "advanced",
+			theme_advanced_buttons1            : "",
+			theme_advanced_buttons2            : "",
+			theme_advanced_buttons3            : "",
+			theme_advanced_toolbar_location    : "none",
+			theme_advanced_toolbar_align       : "left",
+			theme_advanced_statusbar_location  : "bottom",  // activated so we have a resize button
+			theme_advanced_path                : false,     // don't display path in status bar
+			theme_advanced_resizing            : true,
+			theme_advanced_resizing_use_cookie : false,
+			/* disable the gecko spellcheck since AtD provides one */
+			gecko_spellcheck                   : false
+		});
+
+         function doit() {
+			 var langCode = document.checkform.lang.value;
+			 //formes: generals/valencianes/balears
+			 var catOptions = $("input[name=formes]:checked").val(); 
+			 //opcions dins formes valencianes
+			 if (catOptions == "formes_valencianes") 
+			 {
+				catOptions = catOptions + "," + $("input[name=accentuacio]:checked").val()
+										+  "," + $("input[name=incoatius]:checked").val()
+										+  "," + $("input[name=incoatius2]:checked").val()
+										+  "," + $("input[name=demostratius]:checked").val(); 
+			 }
+			 // opcions per a les tres variants
+			 catOptions = catOptions + "," + $("input[name=SE_DAVANT_SC]:checked").val()
+									 + "," + $("input[name=CA_UNPAIRED_QUESTION]:checked").val();
+			 
+             save_cookie_status();
+             
+             tinyMCE.activeEditor.execCommand('mceWritingImprovementTool', langCode, catOptions);
+		 }
+         
+         function read_cookie_status() 
+         {
+            if($.getCookie('sc-languagetool'))
+            {
+                var formes = $.getMetaCookie('formes',SC_COOKIE);
+                
+                if($('#check_formes_valencianes').is(':checked'))
+                {
+                    $('#opcions_valencia').show();
+                } else {
+                    $('#opcions_valencia').hide();
+                }
+                
+                var mesopcions = $.getMetaCookie('mesopcions',SC_COOKIE);
+                if(mesopcions) 
+                {
+                    $('#mesopcions').attr('checked','checked');
+                    $("#mes_opcions").toggle($('#mesopcions').is(':checked'));
+                }
+                
+                var regles_amb_checkbox = Array('SE_DAVANT_SC','CA_UNPAIRED_QUESTION');
+                
+                $.each(regles_amb_checkbox,function(index,nom) {
+					
+					var regla = $.getMetaCookie(nom,SC_COOKIE);
+					
+					if(regla !== undefined)
+					{
+						if(regla>0)
+						{
+							$('input[name='+nom+']').attr('checked','checked');
+						} else {
+							$('input[name='+nom+']').removeAttr('checked');
+						}
+					}
+				});
+                
+                var regles_amb_radio = Array('accentuacio','incoatius','incoatius2','demostratius');
+                
+                $.each(regles_amb_radio,function(index,nom) {
+					
+					var valor = $.getMetaCookie(nom,SC_COOKIE);
+					
+					if(valor !== undefined)
+					{
+						$('[type="radio"][name="'+nom+'"][value="'+valor+'"]').attr('checked','checked');
+					}
+				});
+            }
+         }
+         
+         function save_cookie_status()
+         {
+            if(!$.getCookie(SC_COOKIE))
+            {
+                $.setCookie(SC_COOKIE,'');
+            }
+            
+            var formes = $("input[name=formes]:checked").val();
+            var mesopcions = $('#mesopcions').is(':checked');
+            var se_davant_sc = $('#SE_DAVANT_SC').is(':checked');
+            
+            
+            $.setMetaCookie('formes',SC_COOKIE,formes);
+            $.setMetaCookie('mesopcions',SC_COOKIE,mesopcions);
+            
+            var regles_amb_checkbox = Array('SE_DAVANT_SC','CA_UNPAIRED_QUESTION');
+                
+			$.each(regles_amb_checkbox,function(index,nom) {
+				var valor = $('input[name='+nom+']:checked').val();
+				
+				if(valor)
+				{
+					$.setMetaCookie(nom,SC_COOKIE,1);
+				}
+				else
+				{
+					$.setMetaCookie(nom,SC_COOKIE,-1);
+				}
+			});
+			
+			var regles_amb_radio = Array('accentuacio','incoatius','incoatius2','demostratius');
+                
+            $.each(regles_amb_radio,function(index,nom) {
+				var valor = $('[type="radio"][name="'+nom+'"]:checked').val();
+				$.setMetaCookie(nom,SC_COOKIE,valor);
+			});
+         }
+	 }(jQuery));
+		
+function insertDemoText()
+{
+   var myDemoText="<p>Aquests frases servixen per a probar algun de les errades que detecta el corrector gramaticals. Proveu les variants de flexió verbal: penso, pense, pens. L'accentuació valenciana o general: café o cafè. Paraules errònies segons el context: Et menjaràs tots els canalons? Li va infringir un càstig sever. Errors de sintaxi: la persona amb la que vaig parlar. I algunes altres opcions: Quan es celebrarà la festa?</p>";
+   tinyMCE.activeEditor.setContent(myDemoText);   
+}
