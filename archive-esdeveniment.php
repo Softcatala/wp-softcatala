82718be27bd9d52f8cf29556f5672e1119c247fd
Pau Iranzo <paugnu@gmail.com>
2015-10-27 07:27:21 +0100

    #13 changes done for pull request

--- archive-esdeveniment.php
+++ archive-esdeveniment.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * The template for displaying Archive pages.
- *
- * Used to display archive-type pages if nothing more specific matches a query.
- * For example, puts together date-based pages if no date.php file exists.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since   Timber 0.2
- */
 
 $templates = array( 'archive-esdeveniment.twig' );
 
@@ -27,23 +13,6 @@ $esdeveniments = get_posts($args);
 $post = Timber::query_post($esdeveniments[0]->ID);
 $data['post'] = $post;
 
-$data['title'] = 'Archive';
-if ( is_day() ) {
-    $data['title'] = 'Archive: '.get_the_date( 'D M Y' );
-} else if ( is_month() ) {
-    $data['title'] = 'Archive: '.get_the_date( 'M Y' );
-} else if ( is_year() ) {
-    $data['title'] = 'Archive: '.get_the_date( 'Y' );
-} else if ( is_tag() ) {
-    $data['title'] = single_tag_title( '', false );
-} else if ( is_category() ) {
-    $data['title'] = single_cat_title( '', false );
-    $data['cat_link'] = get_category_link( get_query_var('cat') );
-    array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
-} else if ( is_post_type_archive() ) {
-    $data['title'] = post_type_archive_title( '', false );
-    array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
-}
 $data['links'] = $post->get_field( 'link' );
 $data['sidebar_top'] = Timber::get_widgets('sidebar_top');
 $data['sidebar_bottom'] = Timber::get_widgets('sidebar_bottom');
--- single-esdeveniment.php
+++ single-esdeveniment.php
@@ -2,17 +2,11 @@
 /**
  * The Template for displaying an event
  *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since    Timber 0.1
  */
 
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
-$post_links = types_child_posts('link', $post->ID);
 $context['links'] = $post->get_field( 'link' );
 
 Timber::render( 'esdeveniment.twig', $context );
