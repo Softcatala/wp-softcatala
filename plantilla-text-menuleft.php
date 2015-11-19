802074e8039761ca0f7f6e3f80d50ec3f0a9d855
Pau Iranzo <paugnu@gmail.com>
2015-11-19 15:44:50 +0100

    #58 Remove unused variable links

--- plantilla-text-menuleft.php
+++ plantilla-text-menuleft.php
@@ -11,7 +11,6 @@ $parent_data = get_page_parent_title( $post );
 $context['parent_title'] = $parent_data['title'];
 $context['page_hierarchy'] = get_parent_page_hierarchy($parent_data['id']);
 $context['post'] = $post;
-$context['links'] = $post->get_field( 'link' );
 Timber::render( array( 'plantilla-text-menuleft.twig' ), $context );
 
 
