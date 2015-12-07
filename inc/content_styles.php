51c050035e30cbf7402a0133e57b34d28334e5b0
Pau Iranzo <paugnu@gmail.com>
2015-12-07 12:52:31 +0100

    #72 set default styles for tables and lists inside <section> tags, add main post image

--- inc/content_styles.php
+++ inc/content_styles.php
@@ -1,32 +1,8 @@
 <?php
 
 /**
- * Register our sidebars and widgetized areas.
+ * Replaces the wpautop function with the specific one for Softcatalà
  *
- */
-
-add_filter( 'the_content', 'sc_add_custom_table_class' );
-function sc_add_custom_table_class( $content ) {
-    //General style rewrite for <table> and <ul>
-    $content = str_replace( '<table>', '<table class="table table-bordered">', $content );
-    $content = str_replace( '<ul>', '<ul class="cont-llista">', $content );
-
-    //Styles to rewrite classes from old content in mediawiki
-    $content = str_replace( '<table class="comparacio">', '<table class="table table-bordered">', $content );
-    $content = str_replace( '<table class="prettytable">', '<table class="table table-bordered taula-2col">', $content );
-    $content = str_replace( '<table class="prettytable1">', '<table class="table table-bordered taula-2col">', $content );
-
-    return $content;
-}
-
-/**
- * Replaces double line-breaks with paragraph elements.
- *
- * A group of regex replaces used to identify text formatted with newlines and
- * replace double line-breaks with HTML paragraph tags. The remaining line-breaks
- * after conversion become <<br />> tags, unless $br is set to '0' or 'false'.
- *
- * @since 0.71
  *
  * @param string $pee The text which has to be formatted.
  * @param bool   $br  Optional. If set, this will convert all remaining line-breaks
