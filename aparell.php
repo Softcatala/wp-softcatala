729df2b5b418fd89c1199e47687330dcd7b19e02
Pau Iranzo <paugnu@gmail.com>
2015-12-11 16:57:03 +0100

    #11 work on the 'aparells' list, comments, logos

--- aparell.php
+++ aparell.php
@@ -11,6 +11,8 @@ $parent_data = get_page_parent_title( $post );
 $context['parent_title'] = $parent_data['title'];
 $context['page_hierarchy'] = get_parent_page_hierarchy($parent_data['id'], 'DESC');
 $context['post'] = $post;
+$args = array( 'post_type' => 'aparell' );
+$context['aparells'] = Timber::get_posts($args);
 Timber::render( array( 'aparells.twig' ), $context );
 
 
