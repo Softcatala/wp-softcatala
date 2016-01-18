f46dbba747ef73aa7b21a514bc8464882c6c72fc
Xavi Ivars <xavi.ivars@gmail.com>
2016-01-18 21:49:21 +0100

    Extracting logic to add draft content

--- inc/ajax_operations.php
+++ inc/ajax_operations.php
@@ -14,7 +14,29 @@ function sc_send_aparell() {
     $versio = sanitize_text_field( $_POST["versio"] );
     $traduccio_catala = sanitize_text_field( $_POST["traduccio_catala"] );
     $correccio_catala = sanitize_text_field( $_POST["correccio_catala"] );
+    $slug = sanitize_title_with_dashes( $nom );
+
+    // comentari no s'utilitza
     $comentari = stripslashes( sanitize_text_field( $_POST["comentari"] ) );
+
+    $terms = array(
+        'tipus_aparell' => array($tipus_aparell),
+        'sistema_operatiu_aparell' => array($sistema_operatiu)
+    );
+
+    $metadata = array(
+        'versio' => $versio,
+        'fabricant' => $fabricant,
+        'conf_cat' => $traduccio_catala,
+        'correccio_cat' => $correccio_catala );
+
+    $success = sc_add_draft_content('aparell', $nom, $slug, $terms, $metadata);
+
+    echo $success;
+    wp_die();
+}
+
+function sc_add_draft_content ( $type, $title, $slug, $allTerms, $metadata) {
 
     //Generate array data
     $post_data = array (
@@ -23,7 +45,7 @@ function sc_send_aparell() {
         'comment_status'	=>	'open',
         'ping_status'		=>	'closed',
         'post_author'		=>	get_current_user_id(),
-        'post_name'		    =>	sanitize_title_with_dashes( $nom ),
+        'post_name'		    =>	$slug,
         'post_title'		=>	$nom,
         'post_date'         => date('Y-m-d H:i:s')
     );
@@ -32,29 +54,26 @@ function sc_send_aparell() {
     if( $post_id ) {
         global $wpcf;
 
-        //Set categories
-        wp_set_post_terms($post_id, array($tipus_aparell), 'tipus_aparell');
-        wp_set_post_terms($post_id, array($sistema_operatiu), 'sistema_operatiu_aparell');
-
-        //Set wpcf fields
-        $wpcf_values = array( 'versio' => $versio, 'fabricant' => $fabricant, 'conf_cat' => $traduccio_catala, 'correccio_cat' => $correccio_catala );
-        foreach ( $wpcf_values as $k => $value ) {
-            $wpcf->field->set( $post_id, $k );
-            $wpcf->field->save( $value );
+        foreach($allTerms as $taxonomy => $terms) {
+            wp_set_post_terms($post_id, $terms, $taxonomy);
         }
 
-        set_featured_image($post_id);
+        foreach($metadata as $meta_key => $meta_value ) {
+            $wpcf->field->set( $post_id, $meta_key );
+            $wpcf->field->save( $meta_value );
+        }
+
+        sc_set_featured_image($post_id);
 
         $success = true;
     } else {
         $success = false;
     }
 
-    echo $success;
-    wp_die();
+    return $success;
 }
 
-function set_featured_image($post_id) {
+function sc_set_featured_image($post_id) {
     $tmpfile = $_FILES['file'];
 
     $upload_overrides = array( 'test_form' => false );
