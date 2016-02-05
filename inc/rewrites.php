6f815e5263c17ea8e6b6ab0b3eca88240e3de3e3
Pau Iranzo <paugnu@gmail.com>
2016-02-05 18:13:25 +0100

    #95 change url + small code fixes

--- inc/rewrites.php
+++ inc/rewrites.php
@@ -2,7 +2,8 @@
 // hook add_rewrite_rules function into rewrite_rules_array
 add_filter('rewrite_rules_array', 'add_rewrite_rules');
 function add_rewrite_rules($aRules) {
-    $aNewRules = array('diccionari_de_sinonims/paraula/([^/]+)/?' => 'index.php?post_type=page&pagename=diccionari_de_sinonims&paraula=$matches[1]');
+    //Diccionari de sinònims
+    $aNewRules = array('diccionari-de-sinonims/paraula/([^/]+)/?' => 'index.php?post_type=page&pagename=diccionari-de-sinonims&paraula=$matches[1]');
     $aRules = $aNewRules + $aRules;
     return $aRules;
 }
