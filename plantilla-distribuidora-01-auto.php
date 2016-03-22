7ef77cd981e0136e69f38c6dd6a1c3c4d742f280
Pau Iranzo <paugnu@gmail.com>
2016-03-22 17:21:43 +0100

    #159 add generic image when none exist

--- plantilla-distribuidora-01-auto.php
+++ plantilla-distribuidora-01-auto.php
@@ -19,12 +19,18 @@ $all_children = get_posts(
 
 $context['links'] = array_map( function ( $post ) {
     $thumb_id = get_post_thumbnail_id( $post );
-    $thumb_url = wp_get_attachment_image_src( $thumb_id,'thumbnail-size', true );
+    if($thumb_id) {
+        $thumb_url = wp_get_attachment_image_src( $thumb_id,'thumbnail-size', true );
+        $image_url = $thumb_url[0];
+    } else {
+        $image_url = get_template_directory_uri() . '/static/images/content/generic_image_bloc.png';
+    }
+
 
     return array(
         'link_url'         => get_permalink( $post ),
         'link_title'       => get_the_title( $post ),
-        'link_image'       => $thumb_url[0],
+        'link_image'       => $image_url,
         'link_description' => get_post_field('post_excerpt', $post)
     );
 } , $all_children );
