10169ab14b7583bbf48d02d06de35e9577cc0fc2
Xavi Ivars <xavi.ivars@gmail.com>
2016-04-07 00:30:18 +0200

    Registering RSS link tag to wp_head

--- planeta.php
+++ planeta.php
@@ -7,15 +7,19 @@
 
 global $xv_planeta;
 
+if ( isset( $xv_planeta) ) {
+	$xv_planeta->register_rss_link();
+	$gent = $xv_planeta->get_user_list();
+	$feed = $xv_planeta->get_feed();
+} else {
+	$gent = array();
+	$feed = array();
+}
+
 $context = Timber::get_context();
 $context['post'] = new TimberPost();
 
-if ( isset( $xv_planeta) ) {
-	$context['gent'] = $xv_planeta->get_user_list();
-	$context['feed'] = $xv_planeta->get_feed();
-} else {
-	$context['gent'] = array();
-	$context['feed'] = array();
-}
+$context['gent'] = $gent;
+$context['feed'] = $feed;
 
 Timber::render(array('planeta.twig'), $context);
