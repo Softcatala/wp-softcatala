df0b112b90ac36f16ba132cd1fe01717dbd4b3c0
Xavi Ivars <xavi.ivars@gmail.com>
2016-08-02 22:54:21 +0200

    v0.8.1

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('WP_SOFTCATALA_VERSION', '0.8.0');
+define('WP_SOFTCATALA_VERSION', '0.8.1');
 
 if ( ! class_exists( 'Timber' ) && is_admin() ) {
     add_action( 'admin_notices', function() {
