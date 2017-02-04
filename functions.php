3321cae799f40786221c19487bec3854f9a37cb4
Xavi Ivars <xavi.ivars@gmail.com>
2017-02-04 21:36:42 +0100

    Bump version

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('WP_SOFTCATALA_VERSION', '0.9.17');
+define('WP_SOFTCATALA_VERSION', '0.9.18');
 
 if ( ! class_exists( 'Timber' ) && is_admin() ) {
     add_action( 'admin_notices', function() {
