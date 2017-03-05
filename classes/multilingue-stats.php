feed2a2c08f0e6aab4c7aaf994f1f0e3f5d4ce43
Xavi Ivars <xavi.ivars@gmail.com>
2017-03-05 13:03:49 +0100

    Code style + New wp-softcatala version

--- classes/multilingue-stats.php
+++ classes/multilingue-stats.php
@@ -9,53 +9,57 @@
 class SC_Multilingue_Stats {
 
 	public function __construct() {
-
 	}
 
 	public function load() {
-
 		$url_api = get_option( 'api_diccionari_multilingue' );
 
-		$api_call = do_json_api_call( $url_api . '/statistics' );
+		$api_call   = do_json_api_call( $url_api . '/statistics' );
 		$statistics = json_decode( $api_call );
 
 		$stats = '';
 
 		if ( $statistics ) {
-			$data = array();
-
-			$data['ca_labels'] = $this->count( $statistics, 'ca_labels' );
-			$data['ca_descs'] = $this->count( $statistics, 'ca_descs' );
-			$data['en_labels'] = $this->count( $statistics, 'en_labels' );
-			$data['en_descs'] = $this->count( $statistics, 'en_descs' );
-			$data['fr_labels'] = $this->count( $statistics, 'fr_labels' );
-			$data['fr_descs'] = $this->count( $statistics, 'fr_descs' );
-			$data['de_labels'] = $this->count( $statistics, 'de_labels' );
-			$data['de_descs'] = $this->count( $statistics, 'de_descs' );
-			$data['es_labels'] = $this->count( $statistics, 'es_labels' );
-			$data['es_descs'] = $this->count( $statistics, 'es_descs' );
-			$data['it_labels'] = $this->count( $statistics, 'it_labels' );
-			$data['it_descs'] = $this->count( $statistics, 'it_descs' );
-
-			$data['date'] = $statistics->wikidata->date;
-			$data['images'] = $statistics->wikidata->images;
-
-			$stats = Timber::fetch( 'ajax/multilingue-stats.twig', array( 'statistics' => $data ) );
+			$stats = $this->render_html( $statistics );
 		}
 
 		return $stats;
 	}
 
-	function count($statistics, $key) {
+	function count( $statistics, $key ) {
 
-		$wikidata = (array) $statistics->wikidata;
+		$wikidata       = (array) $statistics->wikidata;
 		$wikidictionary = (array) $statistics->wikidictionary;
-		$value = $wikidata[ $key ];
+		$value          = $wikidata[ $key ];
 
 		if ( isset( $wikidictionary[ $key ] ) && $wikidictionary[ $key ] ) {
-				$value += $wikidictionary[ $key ];
+			$value += $wikidictionary[ $key ];
 		}
 
 		return $value;
+	}
+
+	public function render_html( $statistics ) {
+		$data = array();
+
+		$data['ca_labels'] = $this->count( $statistics, 'ca_labels' );
+		$data['ca_descs']  = $this->count( $statistics, 'ca_descs' );
+		$data['en_labels'] = $this->count( $statistics, 'en_labels' );
+		$data['en_descs']  = $this->count( $statistics, 'en_descs' );
+		$data['fr_labels'] = $this->count( $statistics, 'fr_labels' );
+		$data['fr_descs']  = $this->count( $statistics, 'fr_descs' );
+		$data['de_labels'] = $this->count( $statistics, 'de_labels' );
+		$data['de_descs']  = $this->count( $statistics, 'de_descs' );
+		$data['es_labels'] = $this->count( $statistics, 'es_labels' );
+		$data['es_descs']  = $this->count( $statistics, 'es_descs' );
+		$data['it_labels'] = $this->count( $statistics, 'it_labels' );
+		$data['it_descs']  = $this->count( $statistics, 'it_descs' );
+
+		$data['date']   = $statistics->wikidata->date;
+		$data['images'] = $statistics->wikidata->images;
+
+		$stats = Timber::fetch( 'ajax/multilingue-stats.twig', array( 'statistics' => $data ) );
+
+		return $stats;
 	}
 }
