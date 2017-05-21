1719ce2d1164d60980f8b32df0bfc0ffe388e7bd
Xavi Ivars <xavi.ivars@gmail.com>
2017-05-21 18:19:06 +0200

    Fixing typo in widget

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '0.9.26' );
+define( 'WP_SOFTCATALA_VERSION', '0.9.27' );
 
 if ( ! class_exists( 'Timber' ) && is_admin() ) {
 	add_action( 'admin_notices', function () {
