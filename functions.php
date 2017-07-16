9ec962c6d71d9c00eb7648f80ae6eb0d624e2614
Xavi Ivars <xavi.ivars@gmail.com>
2017-07-16 19:05:17 +0200

    Bumping version

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '0.9.54' );
+define( 'WP_SOFTCATALA_VERSION', '0.9.55' );
 
 if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
