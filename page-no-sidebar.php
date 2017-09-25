9830da9655f389634c8c20892b42fd2b965fb304
Xavi Ivars <xavi.ivars@gmail.com>
2017-09-25 20:54:34 +0200

    Pàgina sense menú dret

--- page-no-sidebar.php
+++ page-no-sidebar.php
@@ -1,16 +1,14 @@
 <?php
 /**
- * Template Name: Plantilla text amb menú dreta
+ * Template Name: Plantilla text sense menú
  *
  * @package wp-softcatala
  */
 
 $context = Timber::get_context();
 $post = new TimberPost();
-$context['sidebar_top'] = Timber::get_widgets('sidebar_top');
-$context['sidebar_elements'] = array( 'static/dubte_forum.twig', 'baixades.twig', 'links.twig' );
-$context['sidebar_bottom'] = Timber::get_widgets('sidebar_bottom');
 $context['post'] = $post;
 $context['credits'] = $post->get_field( 'credits' );
+$context['excludeSidebar'] = true;
 $context['customAnalytics'] = empty($post->get_field( 'custom_analytics' )) ? false : $post->get_field( 'custom_analytics' );
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
