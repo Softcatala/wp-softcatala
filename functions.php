26b22ba73bf37f345fb8d46d6eb74c76b0d30966
Xavi Ivars <xavi.ivars@gmail.com>
2018-11-28 21:20:03 +0100

    0.9.78

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '0.9.76' );
+define( 'WP_SOFTCATALA_VERSION', '0.9.78' );
 
 if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
