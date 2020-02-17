7ae2a98989aba85555bef9d90ebba4f43e798da1
Miquel <miquelp@gmail.com>
2020-02-17 23:04:53 +0100

    0.9.95 testing develop. envir.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '0.9.94' );
+define( 'WP_SOFTCATALA_VERSION', '0.9.95' );
 
 if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
