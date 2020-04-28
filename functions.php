a340058811f76668bcb7492aa95987e00187b419
Miquel <miquelp@gmail.com>
2020-04-28 23:47:58 +0200

    1.0.09

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '1.0.08' );
+define( 'WP_SOFTCATALA_VERSION', '1.0.09' );
 
 if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
