a04f0321f827f9ec59962a74996398538ac33272
Miquel Piulats <miquel@softcatala.org>
2020-05-04 12:10:45 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '1.0.23' );
+define( 'WP_SOFTCATALA_VERSION', '1.0.24' );
 
 if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
