3762a574e8489c1ea9cd9ba9e9eaba48363de635
Miquel <miquelp@gmail.com>
2020-08-05 21:02:44 +0200

    1.0.38

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '1.0.37' );
+define( 'WP_SOFTCATALA_VERSION', '1.0.38' );
 
 include ('php73.php');
 
--- static/js/neuronal/main.js
+++ static/js/neuronal/main.js
@@ -4,9 +4,9 @@
 
 var neuronal_json_url = "https://www.softcatala.org/sc/v2/api/nmt-engcat";
 
-var neuronalVista = ( function (){
+var neuronalVista = (function () {
 
-    
+
     var elementsDOM = {
         btncatsource: '#origin-cat',
         btncattarget: '#target-cat',
@@ -14,7 +14,7 @@ var neuronalVista = ( function (){
         btnengtarget: '#target-en',
         btnswitch: '.direccio',
         btntrad: '#translate',
-        firsttext: '.primer-textarea',  
+        firsttext: '.primer-textarea',
         secondtext: '.second-textarea',
         time: '#time',
         btncopy: '#btncopy',
@@ -27,168 +27,187 @@ var neuronalVista = ( function (){
         file: '#n_file',
         model_name: '#n_model_name',
         btntradfile: '#translate_file',
+        info: '#info',
+        error: '#error',
+        errormessage: '#errormessage'
     }
 
     var direction;
-    
-    
+
+
     return {
 
-        elementsDOM: function(){
+        elementsDOM: function () {
             return elementsDOM;
         },
-        initDOM: function(){
-            
-            document.querySelector(elementsDOM.btntrad).disabled = true;
+        initDOM: function () {
+
+            jQuery(elementsDOM.btntrad).prop('disabled', true);
             //document.querySelector(elementsDOM.btncopy).disabled = true;
 
             document.querySelector(elementsDOM.btntrad).style.width = '130px';
             //document.querySelector(elementsDOM.btncopy).style.marginTop = '10px';
-            
+            //document.querySelector(elementsDOM.btntradfile).style.margin = '230px';
+
             jQuery(elementsDOM.slsourcemob).selectpicker();
             jQuery(elementsDOM.sltargetmob).selectpicker();
+            jQuery(elementsDOM.info).hide();
+            jQuery(elementsDOM.error).hide();
 
             this.switchEngCat();
-                 
-            
+
         },
-        switch: function(){
-            //document.querySelector(elementsDOM.btncatsource).classList.toggle('select');
-            //document.querySelector(elementsDOM.btnengsource).classList.toggle('select');
-            //document.querySelector(elementsDOM.btncattarget).classList.toggle('select');
-            //document.querySelector(elementsDOM.btnengtarget).classList.toggle('select');
+        switch: function () {
 
-            firsttext = document.querySelector(elementsDOM.firsttext).value;
-            secondtext = document.querySelector(elementsDOM.secondtext).innerHTML;
+            firsttext = jQuery(elementsDOM.firsttext).val();
+            secondtext = jQuery(elementsDOM.secondtext).html();
 
-            document.querySelector(elementsDOM.firsttext).value = secondtext;
-            document.querySelector(elementsDOM.secondtext).innerHTML = firsttext;
-            
+            jQuery(elementsDOM.firsttext).val(secondtext);
+            jQuery(elementsDOM.secondtext).html(firsttext);
+
             if (direction == 'eng-cat') {
 
                 direction = 'cat-eng';
                 this.switchCatEng();
 
+            } else {
 
-            }else{  
                 direction = 'eng-cat';
-                this.switchEngCat();   
+                this.switchEngCat();
             }
-            
-        },
-        switchCatEng: function(){
-            
-            document.querySelector(elementsDOM.btncatsource).classList.add('select');
-            document.querySelector(elementsDOM.btnengsource).classList.remove('select');
-            document.querySelector(elementsDOM.btncattarget).classList.remove('select');
-            document.querySelector(elementsDOM.btnengtarget).classList.add('select');
 
-            //document.querySelector(elementsDOM.slsourcemob).value = 'cat';
-            //document.querySelector(elementsDOM.sltargetmob).value = 'eng';
-            
+        },
+        switchCatEng: function () {
+
+            jQuery(elementsDOM.btncatsource).addClass('select');
+            jQuery(elementsDOM.btnengsource).removeClass('select');
+            jQuery(elementsDOM.btncattarget).removeClass('select');
+            jQuery(elementsDOM.btnengtarget).addClass('select');
+
             jQuery(elementsDOM.slsourcemob).selectpicker('val', 'cat');
             jQuery(elementsDOM.sltargetmob).selectpicker('val', 'eng');
 
             direction = 'cat-eng';
-            
-            
         },
-        switchEngCat: function(){
-            
-            document.querySelector(elementsDOM.btncatsource).classList.remove('select');
-            document.querySelector(elementsDOM.btnengsource).classList.add('select');
-            document.querySelector(elementsDOM.btncattarget).classList.add('select');
-            document.querySelector(elementsDOM.btnengtarget).classList.remove('select');
+        switchEngCat: function () {
 
-            //document.querySelector(elementsDOM.slsourcemob).value = 'eng';
-            //document.querySelector(elementsDOM.sltargetmob).value = 'cat';
+            jQuery(elementsDOM.btncatsource).removeClass('select');
+            jQuery(elementsDOM.btnengsource).addClass('select');
+            jQuery(elementsDOM.btncattarget).addClass('select');
+            jQuery(elementsDOM.btnengtarget).removeClass('select');
 
             jQuery(elementsDOM.slsourcemob).selectpicker('val', 'eng');
             jQuery(elementsDOM.sltargetmob).selectpicker('val', 'cat');
 
             direction = 'eng-cat';
-            
-            
-        },
-        getDirection: function(){
 
+
+        },
+        getDirection: function () {
             return direction;
         },
-        enableTrad: function(){
+        enableTrad: function () {
 
-            if (document.querySelector(elementsDOM.firsttext).value)
-                document.querySelector(elementsDOM.btntrad).disabled = false
+            if (jQuery(elementsDOM.firsttext))
+                jQuery(elementsDOM.btntrad).prop('disabled', false)
             else
-                document.querySelector(elementsDOM.btntrad).disabled = true
-            
+                jQuery(elementsDOM.btntrad).prop('disabled', true)
+
+
         },
-        updateTrad: function(translation){
-            
-            document.querySelector(elementsDOM.secondtext).innerHTML = translation.translated_text;
-            document.querySelector(elementsDOM.time).innerHTML = translation.time;
-            document.querySelector(elementsDOM.btntrad).innerHTML = "Tradueix";
+        updateTrad: function (translation) {
+
+            jQuery(elementsDOM.secondtext).html(translation.translated_text);
+            jQuery(elementsDOM.time).html(translation.time);
+            jQuery(elementsDOM.btntrad).html("Tradueix");
+
             //document.querySelector(elementsDOM.btncopy).disabled = false;
+        },
+        sentFileAlert: function () {
+
+            jQuery(elementsDOM.error).hide();
+            jQuery(elementsDOM.info).removeClass('hidden');
+            jQuery(elementsDOM.info).show('slow');
+            jQuery(elementsDOM.email).val('');
+            jQuery(elementsDOM.file).val('');
+
+        },
+        displayError: function (errortxt) {
+            jQuery(elementsDOM.info).hide();
+            jQuery(elementsDOM.error).removeClass('hidden');
+            jQuery(elementsDOM.errormessage).html(errortxt);
+            jQuery(elementsDOM.error).show('slow');
         }
-            
+
+
     }
 
 })();
 
-var neuronalApp = ( function (vistaCtrl){
+var neuronalApp = (function (vistaCtrl) {
 
-    var initEventsDoom = function (){
-      
+    var initEventsDoom = function () {
+
         var elementsDOM = vistaCtrl.elementsDOM();
-        
-        
-        document.querySelector(elementsDOM.slsourcemob).addEventListener('change', function(e){
-            
+
+
+        document.querySelector(elementsDOM.slsourcemob).addEventListener('change', function (e) {
+
             if (document.querySelector(elementsDOM.slsourcemob).value == 'eng')
                 vistaCtrl.switchEngCat()
             else
                 vistaCtrl.switchCatEng()
-             
+
         });
-        
-        document.querySelector(elementsDOM.sltargetmob).addEventListener('change', function(e){
-            
+
+        document.querySelector(elementsDOM.sltargetmob).addEventListener('change', function (e) {
+
             if (document.querySelector(elementsDOM.sltargetmob).value == 'eng')
                 vistaCtrl.switchCatEng();
             else
-                vistaCtrl.switchEngCat();   
-             
+                vistaCtrl.switchEngCat();
+
         });
-        
+
         // Event switch
-        document.querySelector(elementsDOM.btnswitch).addEventListener('click', function(e){
+        document.querySelector(elementsDOM.btnswitch).addEventListener('click', function (e) {
             vistaCtrl.switch();
         });
-        document.querySelector(elementsDOM.btncatsource).addEventListener('click', function(e){
+        document.querySelector(elementsDOM.btncatsource).addEventListener('click', function (e) {
             vistaCtrl.switchCatEng();
         });
-        document.querySelector(elementsDOM.btnengsource).addEventListener('click', function(e){
+        document.querySelector(elementsDOM.btnengsource).addEventListener('click', function (e) {
             vistaCtrl.switchEngCat();
         });
-        document.querySelector(elementsDOM.btncattarget).addEventListener('click', function(e){
+        document.querySelector(elementsDOM.btncattarget).addEventListener('click', function (e) {
             vistaCtrl.switchEngCat();
         });
-        document.querySelector(elementsDOM.btnengtarget).addEventListener('click', function(e){
+        document.querySelector(elementsDOM.btnengtarget).addEventListener('click', function (e) {
             vistaCtrl.switchCatEng();
         });
-        
-        document.querySelector(elementsDOM.firsttext).addEventListener('input', function(e){
+
+        document.querySelector(elementsDOM.firsttext).addEventListener('input', function (e) {
             vistaCtrl.enableTrad();
         });
-/*
+
+        /*
         document.querySelector(elementsDOM.btncopy).addEventListener('input', function(e){
             document.execCommand('copy');
             var copytext = document.querySelector(elementsDOM.secondtext).textContent;
             copyToClipBoard(copyText);
         });
-*/
-        document.querySelector(elementsDOM.btntrad).addEventListener('click', function(e){
-        
-           document.querySelector(elementsDOM.btntrad).innerHTML = "<i class=\"fa fa-spinner fa-pulse fa-fw\"></i>";
+
+        */
+        document.querySelector(elementsDOM.error + '> button').addEventListener('click', function (e) {
+            jQuery(elementsDOM.error).hide('slow');
+        });
+        document.querySelector(elementsDOM.info + '> button').addEventListener('click', function (e) {
+            jQuery(elementsDOM.info).hide('slow');
+        });
+
+        document.querySelector(elementsDOM.btntrad).addEventListener('click', function (e) {
+
+            document.querySelector(elementsDOM.btntrad).innerHTML = "<i class=\"fa fa-spinner fa-pulse fa-fw\"></i>";
             var translation = {
                 source_text: document.querySelector(elementsDOM.firsttext).value,
                 direction: vistaCtrl.getDirection(),
@@ -196,30 +215,65 @@ var neuronalApp = ( function (vistaCtrl){
                 time: ""
             }
             translate(translation);
-     
-        });
-        document.querySelector(elementsDOM.btntradfile).addEventListener('click', function(e){
-            
-             var translation = {
-                 file: document.querySelector(elementsDOM.file).files[0],
-                 email: document.querySelector(elementsDOM.email).value,
-                 model_name: document.querySelector(elementsDOM.model_name).value
-             }
-             translate_file(translation);
-      
-         });
-    
-    }
 
-    var translate = function(translation){
- 
+        });
+        document.querySelector(elementsDOM.btntradfile).addEventListener('click', function (e) {
+
+            if (!validateEmail(document.querySelector(elementsDOM.email).value)) {
+
+                vistaCtrl.displayError('Reviseu la vostra adreça electrònica.');
+                document.querySelector(elementsDOM.email).focus();
+
+            } else if (!document.querySelector(elementsDOM.file).files[0]) {
+
+                vistaCtrl.displayError('Cal que trieu un fitxer del vostre ordinador.');
+                document.querySelector(elementsDOM.file).focus();
+
+            } else if (document.querySelector(elementsDOM.file).files[0].size > 256000) {
+
+                vistaCtrl.displayError('La mida màxima és de 256Kb. El vostre fitxer ocupa ' + returnFileSize(document.querySelector(elementsDOM.file).files[0].size) + '.')
+                document.querySelector(elementsDOM.file).focus();
+
+            } else {
+
+                var translation = {
+                    file: document.querySelector(elementsDOM.file).files[0],
+                    email: document.querySelector(elementsDOM.email).value,
+                    model_name: document.querySelector(elementsDOM.model_name).value
+                }
+
+                translate_file(translation);
+
+            }
+
+        });
+
+
+    }
+    /* Helper functions */
+    var validateEmail = function (email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    var returnFileSize = function (number) {
+        if (number < 1024) {
+            return number + 'bytes';
+        } else if (number >= 1024 && number < 1048576) {
+            return (number / 1024).toFixed(1) + 'KB';
+        } else if (number >= 1048576) {
+            return (number / 1048576).toFixed(1) + 'MB';
+        }
+    }
+    /* Tranlating function */
+    var translate = function (translation) {
+
         var xhr = new XMLHttpRequest();
         url = neuronal_json_url + `/translate/`;
         xhr.open('POST', url);
         xhr.setRequestHeader('Content-Type', 'application/json');
 
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {   
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
                 json = JSON.parse(xhr.responseText);
                 translation.translated_text = json["translated"];
                 translation.time = 'Traducció realitzada en un temps de: ' + json["time"];
@@ -234,45 +288,41 @@ var neuronalApp = ( function (vistaCtrl){
         xhr.send(payload);
     }
 
-    var translate_file = function (translation)
-    {
-        
+    var translate_file = function (translation) {
+
         var xmlHttp = new XMLHttpRequest();
-            xmlHttp.onreadystatechange = function()
-            {
-                if(xmlHttp.readyState != 4)
-                {
-                    return;
-                }
-    
-                if (xmlHttp.status == 200)
-                {
-                    alert("En una estona rebeu el fitxer traduÃ¯t per correu electrÃ²nic");
-                }
-                else
-                {
-                    json = JSON.parse(xmlHttp.responseText);
-                    alert(json['error']);
-                }
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState != 4) {
+                return;
             }
-    
-            /*
-            var formData = new FormData(document.getElementById('form-id'));
-            */
-            url = neuronal_json_url + `/translate_file/`;
-            
-            var formData = new FormData();
-            formData.append("email", translation.email);
-            formData.append("model_name", translation.model_name);
-            formData.append("file", translation.file);
-            xmlHttp.open("post", url);
-            xmlHttp.send(formData); 
+
+            if (xmlHttp.status == 200) {
+                vistaCtrl.sentFileAlert();
+            }
+            else {
+                json = JSON.parse(xmlHttp.responseText);
+                vistaCtrl.displayError(json['error']);
+
+            }
+        }
+
+
+        url = neuronal_json_url + `/translate_file/`;
+
+        var formData = new FormData();
+        formData.append("email", translation.email);
+        formData.append("model_name", translation.model_name);
+        formData.append("file", translation.file);
+        xmlHttp.open("post", url);
+        xmlHttp.send(formData);
+
+
     }
-   
+
     return {
 
-        init: function(){
-            
+        init: function () {
+
             vistaCtrl.initDOM();
             initEventsDoom();
             console.log('app iniciada');
