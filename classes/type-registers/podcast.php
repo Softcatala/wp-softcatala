67c93d19135e6de500a4a46289234e739e1fce61
Xavi Ivars <xavi.ivars@gmail.com>
2020-09-05 01:11:20 +0200

    Move to function

--- classes/type-registers/podcast.php
+++ classes/type-registers/podcast.php
@@ -97,17 +97,17 @@ class Podcast extends PostType {
         );
         register_taxonomy( 'podcast-programa', array( 'podcast' ), $args );
 
-        add_filter( 'post_type_link', array($this, 'permalink_podcastprograma'), 1, 3 );
+        add_filter( 'post_type_link', 'permalink_podcastprograma', 10, 2 );
     }
+}
 
-    public function permalink_podcastprograma( $post_link, $id = 0 ){
-        $post = get_post($id);
-        if ( is_object( $post ) ){
-            $terms = wp_get_object_terms( $post->ID, 'podcast-programa' );
-            if( $terms ){
-                return str_replace( '%podcastprograma%' , $terms[0]->slug , $post_link );
-            }
+public function permalink_podcastprograma( $post_link, $id = 0 ){
+    $post = get_post($id);
+    if ( is_object( $post ) ){
+        $terms = wp_get_object_terms( $post->ID, 'podcast-programa' );
+        if( $terms ){
+            return str_replace( '%podcastprograma%' , $terms[0]->slug , $post_link );
         }
-        return $post_link;
     }
+    return $post_link;
 }
