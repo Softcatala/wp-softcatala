6977879378295aa516bbca01861d04e46b752282
Xavi Ivars <xavi.ivars@gmail.com>
2020-10-04 21:26:27 +0200

    Add icons

--- page-memories.php
+++ page-memories.php
@@ -17,6 +17,6 @@ $context['last_generation'] = $generation_data->generation_date;
 /* JS scripts */
 wp_enqueue_script( 'sc-js-chosen', get_template_directory_uri() . '/static/js/memories/chosen.jquery.min.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
 wp_enqueue_script( 'sc-js-memories', get_template_directory_uri() . '/static/js/memories/memories.js', array('sc-js-chosen'), WP_SOFTCATALA_VERSION, true );
-wp_enqueue_style( 'sc-css-chosen', get_template_directory_uri() . '/static/css/chosen.css', array(), WP_SOFTCATALA_VERSION );
+wp_enqueue_style( 'sc-css-chosen', get_template_directory_uri() . '/static/css/memories/chosen.css', array(), WP_SOFTCATALA_VERSION );
 
 Timber::render( array( 'memories.twig' ), $context );
