e5802f9ffb617b8ec7bd97498503162707fadfc9
Xavi Ivars <xavi.ivars@gmail.com>
2020-10-04 21:23:01 +0200

    Add styles & js

--- page-memories.php
+++ page-memories.php
@@ -14,4 +14,8 @@ $generation_data = $sc_memory->get_generation_data();
 $context['projects'] = $generation_data->memories;
 $context['last_generation'] = $generation_data->generation_date;
 
+/* JS scripts */
+wp_enqueue_script( 'sc-js-chosen', get_template_directory_uri() . '/static/js/memories/chosen.jquery.min.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
+wp_enqueue_style( 'sc-css-chosen', get_template_directory_uri() . '/static/css/chosen.css', array(), WP_SOFTCATALA_VERSION );
+
 Timber::render( array( 'memories.twig' ), $context );
