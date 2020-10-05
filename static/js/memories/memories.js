191cbbd36bde172395783023eec6e76e4ccd37d3
Xavi Ivars <xavi.ivars@gmail.com>
2020-10-06 00:32:59 +0200

    Better UI

--- static/js/memories/memories.js
+++ static/js/memories/memories.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function(){
     jQuery(".chosen").chosen();
+    jQuery("#search-samples-area").click(showHelp);
 });
+
+function showHelp() {
+    jQuery('#show-samples').toggle();
+}
