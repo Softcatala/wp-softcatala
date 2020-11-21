f4c85f9bad7cb339e271cdea0dd6a1d18d20b8d8
Xavi Ivars <xavi.ivars@gmail.com>
2020-11-21 11:55:49 +0100

    Improve browser history

--- static/js/memories/memories.js
+++ static/js/memories/memories.js
@@ -174,7 +174,17 @@ function tryPredefinedQuery(u) {
     if (u) {
         s = new URLSearchParams(u)
         fillForm(s.get('source'), s.get('target'), s.get('project'))
-        searchMemories(s.get('source'), s.get('target'), s.get('project'), s.get('pg') || 1);
+        const parsed = Number.parseInt(s.get('pg'));
+        if (Number.isInteger(parsed) && parsed > 1) {
+            const total = parsed;
+            searchMemories(s.get('source'), s.get('target'), s.get('project'), 1);
+            for(let i = 1; i <= total; i++) {
+                jQuery("show-more").click();
+            }
+
+        } else {
+            searchMemories(s.get('source'), s.get('target'), s.get('project'), s.get('pg') || 1);
+        }
     }
 }
 
