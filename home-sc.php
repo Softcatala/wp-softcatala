cf491921ca776fd4c2904628bbf40e7c0d33c4c2
Xavi Ivars <xavi.ivars@gmail.com>
2020-12-21 10:47:41 +0100

    Add podcasts to home page

--- home-sc.php
+++ home-sc.php
@@ -12,11 +12,8 @@ $context['ads_container'] = true;
 
 //Sections population
 $context['slides'] = Timber::get_posts( array( 'post_type' => 'slider' ) );
-$args = array( 'post_type' => 'post', 'numberposts' => '3', 'post_status' => 'publish' );
-$context['posts'] = Timber::get_posts($args);
-add_filter('posts_orderby','orderbyreplace');
-query_posts($args);
-$context['esdeveniments'] = \Softcatala\Providers\Esdeveniments::get_highlights();
-remove_filter('posts_orderby','orderbyreplace');
+$context['posts'] = Timber::get_posts(array( 'post_type' => 'post', 'numberposts' => '3', 'post_status' => 'publish' ));
 $context['programari'] = get_top_downloads_home();
+$context['podcasts'] = Timber::get_posts( array( 'post_type' => 'podcast', 'numberposts' => 1 ) );
+
 Timber::render( $templates, $context );
