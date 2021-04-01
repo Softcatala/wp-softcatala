2abda524b15a606de5c67df9be078e8f1fae85ae
Miquel <miquelp@gmail.com>
2021-04-01 16:27:00 +0200

    Millores corrector

--- corrector.php
+++ corrector.php
@@ -13,6 +13,7 @@ wp_enqueue_script( 'sc-js-corrector-3', get_template_directory_uri() . '/inc/lan
 wp_enqueue_script( 'sc-js-corrector-5', get_template_directory_uri() . '/static/js/languagetool.js', array('sc-js-metacookie'), WP_SOFTCATALA_VERSION, true );
 wp_enqueue_script( 'sc-js-corrector-6', get_template_directory_uri() . '/static/js/jquery.fancybox-2.1.5.pack.js', array('sc-js-main'), '2.1.5', true );
 wp_enqueue_style( 'sc-css-corrector', get_template_directory_uri() . '/static/css/languagetool.css', array(), WP_SOFTCATALA_VERSION );
+wp_enqueue_style( 'sc-css-corrector-stats', get_template_directory_uri() . '/static/css/corrector.css', array(), WP_SOFTCATALA_VERSION );
 wp_localize_script( 'sc-js-corrector-1', 'scajax', array(
     'ajax_url' => admin_url( 'admin-ajax.php' )
 ));
