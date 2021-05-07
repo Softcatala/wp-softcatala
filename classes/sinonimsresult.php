1a2ecb32932097c5b516c89d647775248072feee
Xavi Ivars <xavi.ivars@gmail.com>
2021-05-07 20:39:33 +0200

    Apòstrof

--- classes/sinonimsresult.php
+++ classes/sinonimsresult.php
@@ -20,7 +20,7 @@ class SC_SinonimsResult {
 	 */
 	public function __construct( $status, $html, $canonical_lemma, $canonical = '', $title = '', $content_title = '' ) {
 		$this->status = $status;
-		$this->html = $html.str_replace("'", '’');
+		$this->html = str_replace("'", '’', $html);
 		$this->canonical = $canonical;
 		$this->title = $title;
 		$this->content_title = $content_title;
