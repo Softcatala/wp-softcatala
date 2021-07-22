867b94149521a18880c8924678ae840dc6f12b12
Jordi Mas <jmas@softcatala.org>
2021-07-22 10:08:55 +0200

    Migrate to Jquery

--- static/js/resum-de-textos.js
+++ static/js/resum-de-textos.js
@@ -1,34 +1,37 @@
-var URL='https://api.softcatala.org/summarization-service/v1/'
+var URL='https://api.softcatala.org/summarization-service/v1'
 
 function summarize_text() {
 
     text = document.getElementById('source_text').value;
     num_sentences = document.getElementById('num_sentences').value;
-    
-    var xhr = new XMLHttpRequest();
     url = URL + `/summarize_by_sentence`;
-    xhr.open('POST', url);
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-//            alert(xhr.responseText);
-            json = JSON.parse(xhr.responseText);
+    var post_data = new FormData();
+    post_data.append("text", text);
+    post_data.append("num_sentences", num_sentences);
 
-            element = document.getElementById('translated_text');
-            text = json["summary"];
-            element.value = text;
-
-            element = document.getElementById('time_used');
-            text = json["time"];
-            element.innerText = text;
-
-        }
-    }
-
-    var payload = new FormData();
-    payload.append("text", text);
-    payload.append("num_sentences", num_sentences);
-    xhr.send(payload);
+    jQuery.ajax({
+        url: url,
+        type: 'POST',
+        data: post_data,
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        success : print_results,
+        error : ko_function
+    });
 }
 
+function print_results(result) {
 
+    text = result['summary']
+    jQuery('#summarized_text').val(text)
+
+    text = result['time']
+    jQuery('#time_used').val(text)
+}
+
+function ko_function(result) {
+    text = "S'ha produït un error i no s'ha pogut completar la petició"
+    jQuery('#summarized_text').val(text)
+}
