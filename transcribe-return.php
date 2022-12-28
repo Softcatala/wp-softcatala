3c29bc5b8ccd2319a1457a11202b897cd88e408c
Jordi Mas <jmas@softcatala.org>
2022-12-28 08:27:43 +0100

    Return page for transcription

---
+++ transcribe-return.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Transcribe-return
+ *
+ * @package wp-softcatala
+ */
+wp_enqueue_script( 'sc-js-nombres-soros', get_template_directory_uri() . '/static/js/transcribe.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
+
+$context = Timber::get_context();
+$context['ads_container'] = true;
+$context['sidebar_top'] = Timber::get_widgets('sidebar_top_recursos');
+$context['sidebar_elements'] = array( 'static/ajudeu.twig', 'static/dubte_forum.twig', 'baixades.twig', 'links.twig' );
+$context['sidebar_bottom'] = Timber::get_widgets('sidebar_bottom_recursos');
+$context['post'] = new TimberPost();
+Timber::render( array( 'transcribe-return.twig' ), $context );
+
