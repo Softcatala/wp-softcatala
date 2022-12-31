1602d75ee55cadf83db9f39931a63349925fe9af
Jordi Mas <jmas@softcatala.org>
2022-12-31 06:16:20 +0100

    Rename transcribe-return to transcribe-results

--- transcribe-results.php
+++ transcribe-results.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Template Name: Transcribe-return
+ * Template Name: Transcribe-results
  *
  * @package wp-softcatala
  */
-wp_enqueue_script( 'sc-js-nombres-soros', get_template_directory_uri() . '/static/js/transcribe-return.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
+wp_enqueue_script( 'sc-js-nombres-soros', get_template_directory_uri() . '/static/js/transcribe-results.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
 
 $context = Timber::get_context();
 $context['ads_container'] = true;
@@ -12,5 +12,5 @@ $context['sidebar_top'] = Timber::get_widgets('sidebar_top_recursos');
 $context['sidebar_elements'] = array( 'static/ajudeu.twig', 'static/dubte_forum.twig', 'baixades.twig', 'links.twig' );
 $context['sidebar_bottom'] = Timber::get_widgets('sidebar_bottom_recursos');
 $context['post'] = new TimberPost();
-Timber::render( array( 'transcribe-return.twig' ), $context );
+Timber::render( array( 'transcribe-results.twig' ), $context );
 
