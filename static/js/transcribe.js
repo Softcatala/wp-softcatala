0f99d1f9540d72301c66368d77a1b842eaf15a4d
Miquel Piulats <miquel@softcatala.org>
2023-01-03 19:06:02 +0100

    Transcribe form

--- static/js/transcribe.js
+++ static/js/transcribe.js
@@ -1,63 +1,110 @@
-var URL='https://api.softcatala.org/transcribe-service/v1'
 
-var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
+
+
+
+(function(){ 
+
+    var URL='https://api.softcatala.org/transcribe-service/v1'
+
+    /* Ajustament boto demanar transcripció */
+    jQuery("#i_demana").css("margin-top", "28px");
+
+    /* Listerner per demanar traducció */
+    jQuery( "#i_demana" ).click(function() {
+
+        if (!validateEmail(document.querySelector('#email').value)) {
+
+            display_error('Reviseu la vostra adreça electrònica.');
+            document.querySelector('#email').focus();
+
+        }else if (!jQuery('#file').val()){
+            display_error("No s'ha especificat fitxer")
+            document.querySelector('#file').focus();
+
+        }else{
+       
+            document.querySelector('#i_demana').innerHTML = "<i class=\"fa fa-spinner fa-pulse fa-fw\"></i>";
+            sendFile();
         }
+    });
 
-        anHttpRequest.open( "GET", aUrl, true );
-        anHttpRequest.send( null );
+    document.querySelector('#error' + '> button').addEventListener('click', function (e) {
+        jQuery('#error').hide('slow');
+    });
+
+    document.querySelector('#info' + '> button').addEventListener('click', function (e) {
+        jQuery('#info').hide('slow');
+    });
+
+    var HttpClient = function() {
+        this.get = function(aUrl, aCallback) {
+            var anHttpRequest = new XMLHttpRequest();
+            anHttpRequest.onreadystatechange = function() { 
+                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                    aCallback(anHttpRequest.responseText);
+            }
+    
+            anHttpRequest.open( "GET", aUrl, true );
+            anHttpRequest.send( null );
+        }
     }
-}
 
-var display_ok_file = function(){
-
-        jQuery('#translate_file').html("Demaneu traducció");
+    var display_ok_file = function(){
+        document.querySelector('#i_demana').innerHTML = "Demana la transcripció";
+        
         jQuery('#error').hide();
         jQuery('#info').removeClass('hidden');
         jQuery('#info').show('slow');
         jQuery('#n_email').val('');
         jQuery('#n_file').val('');
 
-}
+    }
 
-var display_error = function(msg){
-
-        jQuery('#translate_file').html("Demaneu traducció");
+    var display_error = function(msg){
+        document.querySelector('#i_demana').innerHTML = "Demana la transcripció";
+        
         jQuery('#info').hide();
         jQuery('#error').removeClass('hidden');
         jQuery('#errormessage').html(msg);
         jQuery('#error').show('slow');
-}
+    }
 
-function sendFile()
-{
-    var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function()
-        {
-            if(xmlHttp.readyState != 4)
+    function sendFile()
+    {
+        var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function()
             {
-                return;
+                if(xmlHttp.readyState != 4)
+                {
+                    return;
+                }
+
+                if (xmlHttp.status == 200)
+                {
+                    display_ok_file();
+                }
+                else
+                {
+                    json = JSON.parse(xmlHttp.responseText);
+                    display_error(json['error']);
+                }
             }
 
-            if (xmlHttp.status == 200)
-            {
-                display_ok_file();
-            }
-            else
-            {
-                json = JSON.parse(xmlHttp.responseText);
-                display_error(json['error']);
-            }
-        }
+            var formData = new FormData(document.getElementById('form-id'));
+            url = URL + `/transcribe_file/`;
+            xmlHttp.open("post", url);
+            xmlHttp.send(formData); 
+    }
 
-        var formData = new FormData(document.getElementById('form-id'));
-        url = URL + `/transcribe_file/`;
-        xmlHttp.open("post", url);
-        xmlHttp.send(formData); 
-}
+    /* Helper functions */
+    var validateEmail = function (email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+}());
+
+
+
 
 
--- transcribe.php
+++ transcribe.php
@@ -4,7 +4,7 @@
  *
  * @package wp-softcatala
  */
-wp_enqueue_script( 'sc-js-nombres-soros', get_template_directory_uri() . '/static/js/transcribe.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
+wp_enqueue_script( 'sc-js-transcribe', get_template_directory_uri() . '/static/js/transcribe.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
 
 $context = Timber::get_context();
 $context['ads_container'] = true;
