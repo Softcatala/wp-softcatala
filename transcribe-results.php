4824f29093eeb1c042c3748d3104270f54850612
Ciaran O'Reilly <ciaran@oreilly.cat>
2023-01-08 23:55:35 +0100

    Add oTranscribe l10n missing files

--- transcribe-results.php
+++ transcribe-results.php
@@ -6,6 +6,7 @@
  */
 wp_enqueue_script( 'sc-js-transcribe-results', get_template_directory_uri() . '/static/js/transcribe-results.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
 wp_enqueue_script( 'sc-js-otranscribe', get_template_directory_uri() . '/static/js/otranscribe.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
+wp_enqueue_script( 'sc-js-otranscribe-l10n', get_template_directory_uri() . '/static/js/otranscribe-l10n.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
 wp_enqueue_style( 'sc-css-otranscribe', get_template_directory_uri() . '/static/css/otranscribe.css', array('sc-css-main'),WP_SOFTCATALA_VERSION );
 
 $context = Timber::get_context();
