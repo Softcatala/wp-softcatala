fb2f341905bfaa42a453428cbf393003f25cefe6
Ciaran O'Reilly <ciaran@oreilly.cat>
2024-10-27 18:17:22 +0100

    1.2.4: import dub editor as module

--- dubbing-results.php
+++ dubbing-results.php
@@ -5,8 +5,18 @@
  * @package wp-softcatala
  */
 wp_enqueue_script( 'sc-js-dubbing-results', get_template_directory_uri() . '/static/js/dubbing-results.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
-wp_enqueue_script( 'sc-js-subdub-editor', get_template_directory_uri() . '/static/js/subdub-editor.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
 wp_enqueue_script( 'sc-js-subdub-editor-ffmpeg', get_template_directory_uri() . '/static/js/subdub-editor-ffmpeg.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
+wp_enqueue_script( 'sc-js-subdub-editor', get_template_directory_uri() . '/static/js/subdub-editor.js', array('sc-js-main', 'sc-js-subdub-editor-ffmpeg'), WP_SOFTCATALA_VERSION, true );
+
+add_filter("script_loader_tag", "add_module_to_subdub_editor", 10, 3);
+function add_module_to_subdub_editor($tag, $handle, $src)
+{
+    if (strpos( $handle, "sc-js-subdub-editor" ) !== false) {
+        $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
+    }
+
+    return $tag;
+}
 
 $context = Timber::get_context();
 $context['ads_container'] = true;
--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '1.2.3' );
+define( 'WP_SOFTCATALA_VERSION', '1.2.4' );
 
 include ('php73.php');
 
