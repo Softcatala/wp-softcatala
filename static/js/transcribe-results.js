c8a84e998d537ade4ff656f9ba307ccddadf0fef
Jordi Mas <jmas@softcatala.org>
2025-08-06 19:37:01 +0200

    Template

--- static/js/transcribe-results.js
+++ static/js/transcribe-results.js
@@ -1,5 +1,86 @@
 var API_URL='https://api.softcatala.org/transcribe-service/v1'
 
+(function(){
+
+    jQuery('#mostra_opcions_esborrat').change(function(){
+        if(jQuery('#mostra_opcions_esborrat').is(':checked')){
+            jQuery('#esborrat_controls').show()
+        } else {
+            jQuery('#esborrat_controls').hide()
+        }
+    });
+
+    var display_error = function(msg){
+        jQuery('#info').hide();
+        jQuery('#error').removeClass('hidden');
+        jQuery('#errormessage').html(msg);
+        jQuery('#error').show('slow');
+    }
+
+    var display_ok_message = function(message){
+
+        jQuery('#info_text1').text(message);
+        jQuery('#error').hide();
+        jQuery('#info').removeClass('hidden');
+        jQuery('#info').show('slow');
+    }
+
+    function deleteFile()
+    {
+        var xmlHttp = new XMLHttpRequest();
+
+            xmlHttp.onreadystatechange = function()
+            {
+                if(xmlHttp.readyState != 4)
+                {
+                    return;
+                }
+                alert(xmlHttp.status);
+                if (xmlHttp.status == 200)
+                {
+                    display_ok_message("El fitxer s'ha esborrat");
+                }
+                else
+                {
+                    json = JSON.parse(xmlHttp.responseText);
+                    display_error(json['error']);
+                }
+            }
+
+            var url = window.location.href;
+            let params = getUrlVars(url);
+            let uuid = params['uuid'];
+            var formData = new FormData(document.getElementById('form-id'));
+            url = API_URL + `/delete_uuid/`;
+            formData.append('uuid', uuid);
+            xmlHttp.open("post", url);
+            xmlHttp.send(formData);
+    }
+
+    /* Listerner per demanar transcripció */
+    jQuery( "#i_esborra" ).click(function() {
+
+        if (!validateEmail(document.querySelector('#email').value)) {
+
+            display_error('Reviseu la vostra adreça electrònica.');
+            document.querySelector('#email').focus();
+
+        }else{
+            deleteFile();
+        }
+    });
+
+    var validateEmail = function (email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    checkLinks();
+    setLinks();
+
+}());
+
+
 function getUrlVars(url) {
     var vars = {};
     var hashes = url.split("?")[1];
@@ -67,6 +148,4 @@ function checkLinks()
 }
 
 
-checkLinks();
-setLinks();
 
