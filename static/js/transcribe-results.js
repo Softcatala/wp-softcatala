d3a4ef616aab617951a0839093b70ade375149de
Jordi Mas <jmas@softcatala.org>
2025-08-09 09:59:10 +0200

    Enable users to delete transcriptions (#367)
    Enable users to delete transcriptions

--- static/js/transcribe-results.js
+++ static/js/transcribe-results.js
@@ -1,4 +1,89 @@
-var API_URL='https://api.softcatala.org/transcribe-service/v1'
+var API_URL='https://api.softcatala.org/transcribe-service/v1';
+
+(function(){
+
+    jQuery('#mostra_opcions_esborrat').change(function(){
+        if(jQuery('#mostra_opcions_esborrat').is(':checked')){
+            jQuery('#esborrat_controls').show()
+        } else {
+            jQuery('#esborrat_controls').hide()
+        }
+    });
+
+    var display_error = function(msg){
+        jQuery('#info').hide();
+        jQuery('#error').removeClass('hidden');
+        jQuery('#errormessage').html(msg);
+        jQuery('#error').show('slow');
+    }
+
+    var display_ok_message = function(message1, message2){
+        jQuery('#info_text1').text(message1);
+        jQuery('#info_text2').text(message2);
+        jQuery('#error').hide();
+        jQuery('#info').removeClass('hidden');
+        jQuery('#info').show('slow');
+    }
+
+    function deleteFile()
+    {
+        var xmlHttp = new XMLHttpRequest();
+
+            xmlHttp.onreadystatechange = function()
+            {
+                if(xmlHttp.readyState != 4)
+                {
+                    return;
+                }
+                if (xmlHttp.status == 200)
+                {
+                    let message1 = "La transcripció i tots els fitxers associats s'han esborrat";
+                    let message2 = "Torneu a recarregar la pàgina.";
+                    display_ok_message(message1, message2);
+
+                    document.getElementById("txt_down").removeAttribute("href");
+                    document.getElementById("srt_down").removeAttribute("href");
+                    document.getElementById("json_down").removeAttribute("href");
+                    jQuery('#editor').hide();
+                }
+                else
+                {
+                    json = JSON.parse(xmlHttp.responseText);
+                    display_error("Error: " + json['error']);
+                }
+            }
+
+            var url = window.location.href;
+            let params = getUrlVars(url);
+            let uuid = params['uuid'];
+            var formData = new FormData(document.getElementById('form-id'));
+            url = API_URL + `/delete_uuid/`;
+            formData.append('uuid', uuid);
+            xmlHttp.open("post", url);
+            xmlHttp.send(formData);
+    }
+
+    jQuery( "#i_esborra" ).click(function() {
+
+        if (!validateEmail(document.querySelector('#email').value)) {
+            display_error('Reviseu la vostra adreça electrònica.');
+            document.querySelector('#email').focus();
+
+        }else{
+            deleteFile();
+        }
+    });
+
+    var validateEmail = function (email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    checkLinks();
+    setLinks();
+
+}());
+
 
 function getUrlVars(url) {
     var vars = {};
@@ -38,6 +123,7 @@ function setLinks()
     document.getElementById("json_down").setAttribute("href", urlSrt);
 }
 
+
 function hide_ui()
 {
     jQuery('#found').hide();
@@ -67,6 +153,4 @@ function checkLinks()
 }
 
 
-checkLinks();
-setLinks();
 
