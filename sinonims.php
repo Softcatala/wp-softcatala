5a8cbc59c220097cef28ed0b71e1f074189f8bd8
Jordi Mas <jmas@softcatala.org>
2025-08-17 20:05:29 +0200

    Sinonims does not use typeahead library

--- sinonims.php
+++ sinonims.php
@@ -7,7 +7,6 @@
 
 /* JS scripts */
 wp_enqueue_script( 'sc-js-contacte', get_template_directory_uri() . '/static/js/contact_form.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
-wp_enqueue_script( 'sc-js-typeahead', get_template_directory_uri() . '/static/js/typeahead.js', array('sc-js-main'), WP_SOFTCATALA_VERSION, true );
 wp_enqueue_script( 'sc-js-sinonims', get_template_directory_uri() . '/static/js/sinonims.js', array(), WP_SOFTCATALA_VERSION, true );
 wp_localize_script( 'sc-js-sinonims', 'scajax', array(
     'ajax_url' => admin_url( 'admin-ajax.php' )
