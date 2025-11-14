8332dc4a2af67b42b5fa5719a2f2117351cdbbb7
Xavi Ivars <xavi.ivars@gmail.com>
2025-11-14 17:22:47 +0100

    Better cache busting

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'WP_SOFTCATALA_VERSION', '2.0.7' );
+define( 'WP_SOFTCATALA_VERSION', '2.0.8' );
 
 if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
