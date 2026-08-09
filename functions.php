237e1715a9203aa6890ee77cd8d37dea9c94252a
Xavi Ivars <xavi.ivars@gmail.com>
2026-08-09 13:35:44 +0200

    Bump version

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define( 'WP_SOFTCATALA_VERSION', '3.0.10' );
+define( 'WP_SOFTCATALA_VERSION', '3.0.11' );
 
 if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
